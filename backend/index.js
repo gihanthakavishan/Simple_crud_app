2caa99ef38349a2296da6562ec84f72b3ee74c1c
gihanthakavishan <gihanthakavishan@gmail.com>
2024-02-23 00:15:40 +0530

    connect db and run the backend server

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from "express"
+import mysql from "mysql"
+
 
 //create express application
 
 const app = express ()
+//connect db
+const db = mysql.createConnection({
+
+    host: "localhost",
+    user:"root",
+    password:"kavi123",
+    database:"practice"
+
+})
+
+//reach backend server
+app.get("/", (req,res)=>{
+    res.json("hello this is backend")
+})
 
 app.listen(8800, ()=>{
     console.log("connected to backend ! 123")
